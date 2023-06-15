4a45b7cf2dcc8a3268d0bd28304a108751a9384e
Ian <ian.odhiambo@student.moringaschool.com>
2023-06-15 12:11:45 +0300

    Completed asingment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const hq = 42
+function distanceFromHqInBlocks(block){
+    let blockAway;
+}
+if (block=>Hq) {
+    return block - Hq
+}
+else if (block =< Hq){
+blockAway = hq - block
+}
+ distanceFromHqInBlocks(43)
+function distanceFromHqInFeet (num){
+ return distanceFromHqInBlocks (num)*264
+}
+console.log(distanceFromHqInFeet(43))
+
+function distanceTravelledInFeet(start , destination){
+    if (start > destination){
+        return(start - destination)*264
+    }eslse if
+}
